1690559c4c081b3d2d7e3405cd87610853152c70
Abiodun <dokun.abraham@gmail.com>
2022-07-22 09:30:58 +0100

    unintall bootstrap

--- 12-http.js
+++ 12-http.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
     } else if (req.url === '/about') {
         res.end('Welcom to our About page')
     }
-    
+    res.end(`<h1>Oops!!!</h1> <p>Error 404: Page Not Found</p> <a href="/">Back to Home Page</a>`)
 });
 
 server.listen(5000)
